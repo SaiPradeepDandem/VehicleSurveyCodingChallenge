e6fdd7ae297801c3f67febfacc9189b66525e69d
sdandem <saipradeep.dandem@gmail.com>
2018-04-05 11:16:16 +1000

    Initial commit.

---
+++ src/test/java/com/sai/vehiclesurvey/AnalyzeVehicleSurveyTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sai.vehiclesurvey;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test class for AnalyzeVehicleSurvey.
+ *
+ * @author sai.dandem
+ */
+public class AnalyzeVehicleSurveyTest {
+
+    @Test
+    public void testShowMe() {
+        AnalyzeVehicleSurvey.showMe();
+        Assert.assertTrue(true);
+    }
+}
